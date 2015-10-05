c1d4a4a3c55014185712b851f64ae4d0d450e1db
jgriff5 <Jenna.griffith@coloradocollege.edu>
2015-10-05 12:33:09 -0700

    Update create_matrix.py

--- create_matrix.py
+++ create_matrix.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
 
-ls= ['store', 'card_id', 'game_type', 'Game_descr', 'game_id', 'play_count']
+cols= ['store', 'card_id', 'game_type', 'Game_descr', 'game_id', 'play_count']
 
+# read in all the data 
 data1 = pd.read_csv('../data/-----', names = cols)
 data2 = pd.read_csv('../data/-----', names = cols)
 data3 = pd.read_csv('../data/-----', names = cols)
@@ -11,6 +12,7 @@ data5 = pd.read_csv('../data/-----', names = cols)
 data6 = pd.read_csv('../data/-----', names = cols)
 data7 = pd.read_csv('../data/-----', names = cols)
 
+# only columns I want for matrix
 dataday1 = data1[['card_id', 'Game_descr', 'play_count']]
 dataday2 = data2[['card_id', 'Game_descr', 'play_count']]
 dataday3 = data3[['card_id', 'Game_descr', 'play_count']]
@@ -29,6 +31,7 @@ dataday5['card_id'] = dataday5['card_id'].map(lambda x : 'day5' + x)
 dataday6['card_id'] = dataday6['card_id'].map(lambda x : 'day6' + x)
 dataday7['card_id'] = dataday7['card_id'].map(lambda x : 'day7' + x)
 
+# merge into one file
 alldays = dataday1.append(dataday2).append(dataday3).append(dataday4).append(dataday5)\
 .append(dataday6).append(dataday7)
 
